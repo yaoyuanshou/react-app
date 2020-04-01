a9385ffe91fedd7a20065c33f66f3c2b167c67c3
Hades-CheChe <1423814426@qq.com>
2020-04-01 16:10:38 +0800

    test

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default class App extends React.Component{
     render(){
         return (
-            <h1>Hello</h1>
+            <div>
+                <h1>Hello</h1>
+                <h2>world</h2>
+            </div>
         )
     }
 }
