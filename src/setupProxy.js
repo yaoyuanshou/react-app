0a039cc373eac956b3481765fa427b9b8df23a6b
Hades-CheChe <1423814426@qq.com>
2020-04-01 13:59:09 +0800

    项目环境的基本搭建

---
+++ src/setupProxy.js
@@ -0,0 +1,21 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+ 
+module.exports = function(app) {
+ 
+  app.use('/api', createProxyMiddleware({
+    target: 'http://localhost:8099',
+    changeOrigin: true,
+    pathRewrite: { //路径替换
+      // '^/api3': '/api', // axios 访问/douban/v2 == target + /v2
+    }
+  }));
+ 
+  app.use('/mock', createProxyMiddleware({
+    target: 'http://localhost:3333',
+    changeOrigin: true,
+    pathRewrite: { //路径替换
+      '^/mock': '', // axios 访问/douban/v2 == target + /v2
+    }
+  }));
+ 
+};
